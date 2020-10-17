4c9318b786ec8ac194db0c24aee5762bd18a0855
naw linlin min <nodmentor1ygn@gmail.com>
2020-10-17 16:59:54 +0630

    all at once

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,79 @@
+class FeedsController < ApplicationController
+  before_action :set_feed, only: [:show, :edit, :update, :destroy]
+
+  # GET /feeds
+  # GET /feeds.json
+  def index
+    @feeds = Feed.all
+  end
+
+  # GET /feeds/1
+  # GET /feeds/1.json
+  def show
+  end
+
+  # GET /feeds/new
+  def new
+    if params[:back]
+      @feed = Feed.new(feed_params)
+    else
+      @feed = Feed.new
+    end
+  end
+
+  def confirm
+    @feed = Feed.new(feed_params)
+  end
+
+  # GET /feeds/1/edit
+  def edit
+  end
+
+  # POST /feeds
+  # POST /feeds.json
+  def create
+      @feed = Feed.new(feed_params)
+      @feed.user_id = current_user.id
+      if params[:back]
+        render :new
+      else
+        if @feed.save
+          redirect_to feeds_path, notice: "Picture was successfully created."
+        else
+          render :new
+        end
+      end
+    end
+     # PATCH/PUT /feeds/1.json
+  def update
+    respond_to do |format|
+      if @feed.update(feed_params)
+        format.html { redirect_to @feed, notice: 'Picture was successfully updated.' }
+        format.json { render :show, status: :ok, location: @feed }
+      else
+        format.html { render :edit }
+        format.json { render json: @feed.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /feeds/1
+  # DELETE /feeds/1.json
+  def destroy
+    @feed.destroy
+    respond_to do |format|
+      format.html { redirect_to feeds_url, notice: 'Feed was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_feed
+    @feed = Feed.find(params[:id])
+  end
+
+  def feed_params
+    params.require(:feed).permit(:image, :image_cache, :content)
+  end
+end
